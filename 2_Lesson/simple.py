78d119d50c4ee4a6b7eb9501dac9bdbd1629c587
Artem <a.a.yesaulkov@gmail.com>
2026-07-03 10:04:05 +0500

    added python file example

---
+++ 2_Lesson/simple.py
@@ -0,0 +1,6 @@
+def average(a, b):
+    return (a+b)/2
+
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+print(average(first_number, second_number))
