812e730513184fead4ba0bf5b6d59bab09b02a03
Artem <a.a.yesaulkov@gmail.com>
2026-07-03 10:12:33 +0500

    added errors test in c file

--- 2_Lesson/new_simple.c
+++ 2_Lesson/new_simple.c
@@ -10,6 +10,11 @@ int main()
 {
     unsigned int first_number, second_number;
     scanf("%u %u", &first_number, &second_number);
+    if (first_number < 1 || first_number > 1000 || second_number < 1 || second_number > 1000)
+    {
+        printf("invalid input data");
+        return 0;
+    }
     printf("%g", average(first_number, second_number));
     return 0;
 }
